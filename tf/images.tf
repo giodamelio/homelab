8797571cca3aac5959fde9a974e20e191f411e9f
Gio d'Amelio <giodamelio@gmail.com>
2022-03-06 19:04:08 +0000

    Add Portainer image

--- tf/images.tf
+++ tf/images.tf
@@ -1,3 +1,9 @@
 resource "docker_image" "whoami" {
-  name = "docker.io/traefik/whoami:latest"
+  name         = "docker.io/traefik/whoami:latest"
+  keep_locally = false
+}
+
+resource "docker_image" "portainer" {
+  name         = "docker.io/portainer/portainer-ce:2.11.1-alpine"
+  keep_locally = false
 }
--- tf/main.tf
+++ tf/main.tf
@@ -21,13 +21,46 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+# whoami container to use for testing
 resource "docker_container" "test-whoami" {
   image    = docker_image.whoami.latest
   name     = "test-whoami"
   hostname = "test-whoami"
 
   ports {
-    external = "8000"
+    external = "8080"
     internal = "80"
   }
+}
+
+# Portainer for easy management of containers
+resource "docker_container" "portainer" {
+  image    = docker_image.portainer.latest
+  name     = "portainer"
+  hostname = "portainer"
+
+  ports {
+    internal = 8000
+    external = 8000
+  }
+
+  ports {
+    internal = 9443
+    external = 9443
+  }
+
+  volumes {
+    container_path = "/var/run/docker.sock"
+    host_path      = "/var/run/docker.sock"
+  }
+
+  volumes {
+    container_path = "/data"
+    volume_name    = docker_volume.portainer_data.name
+  }
+}
+
+# Volume to store portainers data
+resource "docker_volume" "portainer_data" {
+  name = "portainer_data"
 }
