cee39f8fd7db534f929fc4ebbab0bf1b38216053
Gio d'Amelio <giodamelio@gmail.com>
2022-03-07 00:51:09 +0000

    Get the IP address from a specific network

--- tf/service_test_whoami.tf
+++ tf/service_test_whoami.tf
@@ -12,3 +12,7 @@ resource "docker_container" "test-whoami" {
     name = docker_network.shared.name
   }
 }
+
+output "ip_address" {
+  value = docker_container.test-whoami.network_data[index(docker_container.test-whoami.network_data.*.network_name, "shared")].ip_address
+}
