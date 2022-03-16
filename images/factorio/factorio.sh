9814445e4182f8bc08381f68b769d60b4573fa4e
Gio d'Amelio <giodamelio@gmail.com>
2022-03-16 22:00:29 +0000

    Make the factorio server do the auto-admining. Also make the it listen to RCON for real

--- images/factorio/factorio.sh
+++ images/factorio/factorio.sh
@@ -2,6 +2,7 @@
 
 ./factorio/bin/x64/factorio \
   --start-server /srv/saves/world.zip \
-  --server-settings /srv/server_configs/server-settings.json
+  --server-settings /srv/server_configs/server-settings.json \
+  --server-adminlist /srv/server_configs/server-adminlist.json \
   --rcon-port ${FACTORIO_RCON_PORT} \
   --rcon-password $(cat /srv/server_configs/rcon_password)
