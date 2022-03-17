1b7ed4c24bb7710446cd2d15d6ce7f82b154a42a
Gio d'Amelio <giodamelio@gmail.com>
2022-03-17 17:33:14 +0000

    Create basic proxy-gen container that generates our Caddyfile based on the docker containers

---
+++ tf/service_proxy.tf
@@ -0,0 +1,34 @@
+# Store the hosts file
+resource "docker_volume" "proxy_caddyfile" {
+  name = "proxy_caddyfile"
+}
+
+# Generate the hosts file for the coredns container
+resource "docker_container" "proxy-gen" {
+  image    = docker_image.docker-gen.latest
+  name     = "proxy-gen"
+  hostname = "proxy-gen"
+
+  command = ["-watch", "/etc/docker-gen/templates/Caddyfile.tmpl", "/etc/docker-gen/output/Caddyfile"]
+
+  volumes {
+    container_path = "/tmp/docker.sock"
+    host_path      = "/var/run/docker.sock"
+    read_only      = true
+  }
+
+  volumes {
+    container_path = "/etc/docker-gen/templates"
+    host_path      = abspath("../config/proxy/templates")
+    read_only      = true
+  }
+
+  volumes {
+    container_path = "/etc/docker-gen/output"
+    volume_name    = docker_volume.proxy_caddyfile.name
+  }
+
+  networks_advanced {
+    name = docker_network.shared.name
+  }
+}
